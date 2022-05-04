5bbc7c6239eef1d8ea1e738f6558adee8b70eba9
yokoto <youkotoushi@gmail.com>
2022-05-04 20:48:13 +0900

    add s3

---
+++ s3/main.tf
@@ -0,0 +1,70 @@
+# プライベートバケット
+resource "aws_s3_bucket" "private" {
+  bucket = "private-pragmatic-terraform"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# ブロックパブリックアクセス
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket = aws_s3_bucket.private.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+# パブリックバケット
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform"
+  acl = "public-read"
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+# ログローテーションバケット
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-pragmatic-terraform"
+
+  lifecycle_rule {
+    enabled = true
+  }
+
+  expiration {
+    days = "180"
+  }
+}
+
+# バケットポリシー
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
